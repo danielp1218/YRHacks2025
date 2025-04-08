47e6be6d9c6ec58c2a59c4efb0fa3123e2700ddb
danielp1218 <danielpu2007@gmail.com>
2025-04-08 16:18:42 -0400

    try fix

--- admin-panel/src/util/auth.ts
+++ admin-panel/src/util/auth.ts
@@ -1,11 +1,11 @@
-import {supabase } from '@/util/supabase'
+import { supabase } from '@/util/supabase'
+import { redirect } from 'next/navigation'
 
-
-export const auth = async () =>{
+export const auth = async () => {
     const res = await supabase.auth.getSession()
-    if(res.error){
+    if (res.error || !res.data.session) {
         console.log(res.error);
-        return null;
+        redirect('/signin') // Redirect to sign-in page
     }
     return res.data.session;
 }
